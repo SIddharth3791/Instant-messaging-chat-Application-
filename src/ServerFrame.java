f4b071f2eaa798eadcd2ae12cbae06c677e8a235
siddharth <patelsi14@students.ecu.edu>
2016-03-29 18:34:01 -0400

    Gym-Workout-planner commit

---
+++ src/ServerFrame.java
@@ -0,0 +1,270 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServerFrame extends javax.swing.JFrame {
+  
+ArrayList<String> Clientusers; //Marks:- Holds Clients how get connected
+ArrayList clientOutputStreams;// sets messaging from client in array and sends over.
+
+    public ServerFrame() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ServerScreen = new javax.swing.JTextArea();
+        ConnectButton = new javax.swing.JButton();
+        DisconnectButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        PortnumberTxt = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        ServerScreen.setColumns(20);
+        ServerScreen.setRows(5);
+        jScrollPane1.setViewportView(ServerScreen);
+
+        ConnectButton.setText("Connect ");
+        ConnectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ConnectButtonActionPerformed(evt);
+            }
+        });
+
+        DisconnectButton.setText("Disconnect");
+        DisconnectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DisconnectButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Select Port Number");
+
+        PortnumberTxt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PortnumberTxtActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addComponent(jLabel1)
+                        .addGap(28, 28, 28)
+                        .addComponent(PortnumberTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(83, 83, 83)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(DisconnectButton)
+                            .addComponent(ConnectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(12, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(24, 24, 24)
+                .addComponent(ConnectButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(DisconnectButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(PortnumberTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 17, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+//Marks:- Connection Button will start server
+    private void ConnectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConnectButtonActionPerformed
+        
+        Thread Starter = new Thread(new Starting());
+        Starter.start();
+        ServerScreen.setText("Server started...\n");
+        
+    }//GEN-LAST:event_ConnectButtonActionPerformed
+//marks:- Disconnet button will stop Server for 5 seconds and empty the portnumber Text
+    private void DisconnectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DisconnectButtonActionPerformed
+    try {
+        Thread.sleep(5000); // server will take 5 second hold to stop server.
+        String Empty = "";  
+        PortnumberTxt.setEditable(true);
+        
+    } catch (InterruptedException ex) {
+        Logger.getLogger(ServerFrame.class.getName()).log(Level.SEVERE, null, ex);
+    } 
+    ServerScreen.setText("Server Stopped......\n");
+    }//GEN-LAST:event_DisconnectButtonActionPerformed
+
+    private void PortnumberTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PortnumberTxtActionPerformed
+        
+    }//GEN-LAST:event_PortnumberTxtActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ServerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ServerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ServerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ServerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ServerFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ConnectButton;
+    private javax.swing.JButton DisconnectButton;
+    private javax.swing.JTextField PortnumberTxt;
+    private javax.swing.JTextArea ServerScreen;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+    
+    public class Clients implements Runnable{
+
+       BufferedReader BuffR;
+       Socket socket;
+       PrintWriter PrintW;
+
+    public Clients(Socket sock, PrintWriter clientWrite)
+    {
+      PrintW = clientWrite;
+        
+        try {
+             socket = sock;
+            InputStreamReader Input = new InputStreamReader(socket.getInputStream());
+            BuffR = new BufferedReader(Input);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Clients.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+       
+       
+    @Override
+    public void run() {   
+    
+        String text;
+        String[] data;
+        
+        try {
+            while((text = BuffR.readLine()) != null)
+            {
+                
+               data = text.split("@"); // @ is a divider.
+               
+               
+               if (data[2].equals("Connect"))
+               {
+                  
+                  sendMessage((data[0] + "@" + data[1] + "@" + "Chat"));
+               
+               }else if(data[2].equals("Chat"))
+               {
+                   sendMessage(text);
+               }
+               else
+               {
+                   ServerScreen.append("not Connected");
+               }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Clients.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
+    
+    public void sendMessage(String text)
+    {
+        Iterator getMessage = clientOutputStreams.iterator();
+        
+        while(getMessage.hasNext())
+        {
+            try{
+                PrintWriter Pwriter = (PrintWriter) getMessage.next();
+                Pwriter.println(text);
+                Pwriter.flush();
+            }catch (Exception ex)
+            {
+                System.out.println("error in sending message");
+            }
+        }
+    }
+    
+
+ public class Starting implements Runnable{
+
+        @Override
+        public void run() {
+            
+            clientOutputStreams = new ArrayList();
+            Clientusers = new ArrayList();
+
+            try {
+                final int portnumber = Integer.parseInt(PortnumberTxt.getText());// get portnumber to authenticate,  
+                ServerSocket SSR =  new ServerSocket(portnumber);
+                ServerScreen.append("your new port number is: " +portnumber+"\n");
+                while(true)
+                {
+                    Socket sock = SSR.accept(); // this command accepts socket.
+                    PrintWriter Pwrite = new PrintWriter(sock.getOutputStream()); 
+                    clientOutputStreams.add(Pwrite);
+                    Thread listen = new Thread(new Clients(sock, Pwrite));
+                    listen.start();
+                    ServerScreen.append("Got connection  "+"\n");
+                }
+            } catch (IOException ex) {
+                System.out.println("Wrong connections");
+                
+            }
+        }
+    
+}
+         
+  
+}
---
+++ src/clientFrame.java
@@ -0,0 +1,284 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class clientFrame extends javax.swing.JFrame {
+
+    String username;
+    ArrayList<String> clients = new ArrayList(); // Marks:- Holds all online Clients.
+    int port ;
+    Boolean ConnectionDone = false; //Marks:- boolean to specify if client is connected 
+    
+    Socket socket; //Marks :- Socket to send Message to server through serverSocket
+    BufferedReader BUFFR; //Marks:- 
+    PrintWriter Pwriter;
+    
+    public clientFrame() {
+        initComponents();
+    }
+
+     public void Messaging() 
+    {
+         Thread IncomingReader = new Thread(new SendingMessage());
+         IncomingReader.start();
+    }
+    public void userAdd(String data) 
+    {
+         clients.add(data);
+    }
+    
+     public void writeUsers() 
+    {
+         String[] List = new String[(clients.size())];
+         clients.toArray(List);
+         
+    }
+     
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ChatArea = new javax.swing.JTextArea();
+        TextArea = new javax.swing.JTextField();
+        SendButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        HostnameTxt = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        PortnumberTxt = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        UsernameTxt = new javax.swing.JTextField();
+        ConnectButton = new javax.swing.JButton();
+        disconnectButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        ChatArea.setColumns(20);
+        ChatArea.setRows(5);
+        jScrollPane1.setViewportView(ChatArea);
+
+        SendButton.setText("Send");
+        SendButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SendButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Host Name");
+
+        jLabel2.setText("Port Number ");
+
+        jLabel3.setText("User name");
+
+        ConnectButton.setText("Connect ");
+        ConnectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ConnectButtonActionPerformed(evt);
+            }
+        });
+
+        disconnectButton.setText("Disconnect");
+        disconnectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                disconnectButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(TextArea, javax.swing.GroupLayout.PREFERRED_SIZE, 311, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(SendButton, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(HostnameTxt)
+                    .addComponent(PortnumberTxt)
+                    .addComponent(UsernameTxt, javax.swing.GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(ConnectButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(disconnectButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(37, 37, 37))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(HostnameTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(PortnumberTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(UsernameTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(ConnectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(disconnectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(14, 14, 14)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(TextArea, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(SendButton))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void SendButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SendButtonActionPerformed
+        
+        String nothing = "";
+        if ((TextArea.getText()).equals(nothing)) {
+            TextArea.setText("");
+            TextArea.requestFocus();
+        } else {
+            try {
+               Pwriter.println(username + "@" + TextArea.getText() + "@" + "Chat");
+               Pwriter.flush(); // flushes the buffer
+            } catch (Exception ex) {
+                ChatArea.append("Message was not sent. \n");
+            }
+            TextArea.setText("");
+            TextArea.requestFocus();
+        }
+
+       TextArea.setText("");
+            TextArea.requestFocus();
+        
+        
+    }//GEN-LAST:event_SendButtonActionPerformed
+
+    private void ConnectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConnectButtonActionPerformed
+      
+         if (ConnectionDone == false) 
+        {
+            username = UsernameTxt.getText();
+            UsernameTxt.setEditable(false);
+
+            try 
+            {
+                int portno =Integer.parseInt(PortnumberTxt.getText());
+                final String address = HostnameTxt.getText();
+                socket = new Socket(address, portno);
+                InputStreamReader streamreader = new InputStreamReader(socket.getInputStream()); //marks:- gets Message typed by clients
+                BUFFR = new BufferedReader(streamreader);
+                Pwriter = new PrintWriter(socket.getOutputStream()); //Marks:- sends message to output stream over server 
+                Pwriter.println(username + "@has connected.@Connect");//Marks:- show who is connedted, "@" is a divider, thrid connect tells what to do with the data. 
+                Pwriter.flush(); // Marks:- flushes buffer.
+                ConnectionDone = true; //marks:- checks if client is connected or not.
+            } 
+            catch (Exception ex) 
+            {
+                ChatArea.append("Cannot Connect! Try Again. \n");
+                UsernameTxt.setEditable(true);
+            }
+            
+            Messaging();
+            
+        } else if (ConnectionDone == true) 
+        {
+            ChatArea.append("You are already connected. \n");
+        }
+        
+        
+        
+    }//GEN-LAST:event_ConnectButtonActionPerformed
+
+    private void disconnectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_disconnectButtonActionPerformed
+        
+        try {
+            socket.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(clientFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ConnectionDone = false;
+        
+
+        
+        
+        
+        
+    }//GEN-LAST:event_disconnectButtonActionPerformed
+
+   
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new clientFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea ChatArea;
+    private javax.swing.JButton ConnectButton;
+    private javax.swing.JTextField HostnameTxt;
+    private javax.swing.JTextField PortnumberTxt;
+    private javax.swing.JButton SendButton;
+    private javax.swing.JTextField TextArea;
+    private javax.swing.JTextField UsernameTxt;
+    private javax.swing.JButton disconnectButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+
+
+public class SendingMessage implements Runnable
+{
+
+        @Override
+        public void run() {
+            String[] data;
+            String stream;
+            try 
+            {
+                while ((stream = BUFFR.readLine()) != null) //marks:- check if new data is coming from the server
+                {
+                     data = stream.split("@");
+
+                     if (data[2].equals("Chat")) //mark:- if data[2] which is last data on the array is chat
+                     {
+                        ChatArea.append(data[0] + ": " + data[1] + "\n"); // marks:- data[0] is the username of the person, data[1] is the message.
+                        ChatArea.setCaretPosition(ChatArea.getDocument().getLength());
+                     } 
+                     else if (data[2].equals("Connect"))//mark:- if data[2] which is last data on the array is connect 
+                     {
+                        ChatArea.removeAll();
+                        userAdd(data[0]);
+                     } 
+                     
+                }
+           }catch(Exception ex) { }
+        }
+        }
+    
+}
+
